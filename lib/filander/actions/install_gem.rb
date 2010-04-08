4c7cfd840c1c61753c546821edf46c427be3a889
Johannes J. Schmidt <schmidt@netzmerk.com>
2010-04-08 13:18:07 +0200

    fixed dynamic constant assignment (hopefully)

--- lib/filander/actions/install_gem.rb
+++ lib/filander/actions/install_gem.rb
@@ -4,18 +4,20 @@ module Filander
 
     def install_gem(name, version = nil)
       if version
-        gem_args = [name, version]
         gem_install_args = "-v=#{version} #{name}"
         report_name = "#{name} #{version}"
       else
-        gem_args = [name]
         gem_install_args = name
         report_name = name
       end
       report :install, report_name
       unless Filander.behavior == :pretend
         begin
-          gem *gem_args
+          if version
+            gem name, version
+          else
+            gem name
+          end
         rescue => Gem::LoadError
           cmd "gem install #{gem_install_args}"
         end
