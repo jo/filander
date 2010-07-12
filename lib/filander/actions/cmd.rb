7f554b0d1caa22f3fd188a37afacf592de12fa18
Johannes J. Schmidt <schmidt@netzmerk.com>
2010-07-12 11:47:43 +0200

    enhanced cmd behavior

--- lib/filander/actions/cmd.rb
+++ lib/filander/actions/cmd.rb
@@ -2,10 +2,14 @@ module Filander
   module Cmd
     include Base
 
-    def cmd(command)
-      report :execute, command
+    def cmd(command, verb = :execute, name = command)
+      report verb, name if verb
       unless Filander.behavior == :pretend
-        system command
+        if Filander.verbose
+          system command
+        else
+          `#{command}`
+        end
       end
     end
   end
